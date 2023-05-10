cbe06230027ad2fc41ca32b1a9851f0f758dcb9d
chetanghodasara <chetan.ghodasara@tabist.com>
2023-05-10 11:19:31 +0900

    Use Farday

--- attach_accounts.rb
+++ attach_accounts.rb
@@ -1,23 +1,22 @@
-require 'net/http'
-require 'uri'
+require 'faraday'
 require 'json'
 
 case ARGV[0]
 when 'local'
   coupon_id = 292
-  url = URI.parse("http://localhost:3001/v1/coupons/#{coupon_id}/accounts")
+  url = "http://localhost:3001/v1/coupons/#{coupon_id}/accounts"
   data_file_location = 'data/account_ids_local.txt'
 when 'dev'
   coupon_id = 49
-  url = URI.parse("https://private-internal.internal.dev.tabist.co.jp/v1/coupons/#{coupon_id}/accounts")
+  url = "https://private-internal.internal.dev.tabist.co.jp/v1/coupons/#{coupon_id}/accounts"
   data_file_location = 'data/account_ids_dev.txt'
 when 'stg'
   coupon_id = 99
-  url = URI.parse("https://private-internal.internal.stg.tabist.co.jp/v1/coupons/#{coupon_id}/accounts")
+  url = "https://private-internal.internal.stg.tabist.co.jp/v1/coupons/#{coupon_id}/accounts"
   data_file_location = 'data/account_ids_stg.txt'
 when 'prod'
   coupon_id = 99
-  url = URI.parse("https://private-internal.internal.prod.tabist.co.jp/v1/coupons/#{coupon_id}/accounts")
+  url = "https://private-internal.internal.prod.tabist.co.jp/v1/coupons/#{coupon_id}/accounts"
   data_file_location = 'data/account_ids_prod.txt'
 else
   msgt = 'ERROR: invalid environment argument'
@@ -28,25 +27,29 @@ end
 # Assumption -- the account_ids are unique in the text file.
 account_ids = File.readlines(data_file_location).map(&:strip)
 
+conn = Faraday.new(url: url) do |faraday|
+  faraday.adapter Faraday.default_adapter
+end
+
 puts "########## START #{ARGV[0]} env ##########"
-http = Net::HTTP.new(url.host, url.port)
 
 account_ids.each do |account_id|
   payload = {
     'account_id' => account_id
   }
 
-  request = Net::HTTP::Post.new(url)
-  request['Content-Type'] = 'application/json'
-  request.body = JSON.generate(payload)
-
   puts "Attaching '#{account_id}' to coupon_id #{coupon_id}"
-  response = http.request(request)
+  response = conn.post do |req|
+    req.url url
+    req.headers['Content-Type'] = 'application/json'
+    # req.headers['Authorization'] = "Bearer #{token}"
+    req.body = JSON.generate(payload)
+  end
 
-  if response.code.to_i >= 200 && response.code.to_i < 300
-    puts "-- SUCCESS for '#{account_id}'. Response : #{response.read_body}"
+  if response.success?
+    puts "-- SUCCESS for '#{account_id}'. Response : #{response.body}"
   else
-    puts "-- FAILURE for '#{account_id}' with error code #{response.code}: #{response.read_body}"
+    puts "-- FAILURE for '#{account_id}' with error code #{response.status}: #{response.body}"
   end
 end
 
--- send_emails.rb
+++ send_emails.rb
@@ -1,26 +1,25 @@
-require 'net/http'
-require 'uri'
+require 'faraday'
 require 'json'
 
 case ARGV[0]
 when 'local'
   coupon_code = 'TABIX230315'
-  url = URI('http://localhost:3000/v1/admin/notifications')
+  url = 'http://localhost:3000/v1/admin/notifications'
   token_file_location = 'tokens/token_local.txt'
   data_file_location = 'data/account_ids_local.txt'
 when 'dev'
   coupon_code = 'TABIX230315'
-  url = URI('https://booking.internal.dev.tabist.co.jp/v1/admin/notifications')
+  url = 'https://booking.internal.dev.tabist.co.jp/v1/admin/notifications'
   token_file_location = 'tokens/token_dev.txt'
   data_file_location = 'data/account_ids_dev.txt'
 when 'stg'
   coupon_code = 'TABIX230315'
-  url = URI('https://booking.internal.stg.tabist.co.jp/v1/admin/notifications')
+  url = 'https://booking.internal.stg.tabist.co.jp/v1/admin/notifications'
   token_file_location = 'tokens/token_stg.txt'
   data_file_location = 'data/account_ids_stg.txt'
 when 'prod'
   coupon_code = 'TABIX230315'
-  url = URI('https://booking.internal.prod.tabist.co.jp/v1/admin/notifications')
+  url = 'https://booking.internal.prod.tabist.co.jp/v1/admin/notifications'
   token_file_location = 'tokens/token_prod.txt'
   data_file_location = 'data/account_ids_prod.txt'
 else
@@ -33,7 +32,9 @@ end
 account_ids = File.readlines(data_file_location).map(&:strip)
 token = "Bearer #{File.readlines(token_file_location)[0]}"
 
-puts "token = #{token}"
+conn = Faraday.new(url: url) do |faraday|
+  faraday.adapter Faraday.default_adapter
+end
 
 puts "########## START Sending Emails #{ARGV[0]} env ##########"
 
@@ -48,20 +49,18 @@ account_ids.each do |account_id|
     }
   }
 
-  http = Net::HTTP.new(url.host, url.port)
-
-  request = Net::HTTP::Post.new(url)
-  request['Content-Type'] = 'application/json'
-  request['Authorization'] = token
-  request.body = JSON.generate(payload)
-
   puts "Sending email to '#{account_id}' for coupon_code #{coupon_code}. DateTime #{Time.now.getlocal('+09:00')} JST"
-  response = http.request(request)
+  response = conn.post do |req|
+    req.url url
+    req.headers['Content-Type'] = 'application/json'
+    req.headers['Authorization'] = token
+    req.body = JSON.generate(payload)
+  end
 
-  if response.code.to_i >= 200 && response.code.to_i < 300
-    puts "-- SUCCESS email for '#{account_id}'. Response : #{response.read_body}"
+  if response.success?
+    puts "-- SUCCESS email for '#{account_id}'. Response : #{response.body}"
   else
-    puts "-- FAILURE email for '#{account_id}' with error code #{response.code}: #{response.read_body}"
+    puts "-- FAILURE email for '#{account_id}' with error code #{response.status}: #{response.body}"
   end
 end
 
